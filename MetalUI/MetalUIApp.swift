c5b819bf72baf0276a4ac60434cc69a6627c3a39
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2023-08-15 15:38:02 +0300

    Initial Commit

---
+++ MetalUI/MetalUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalUIApp.swift
+//  MetalUI
+//
+//  Created by Aleksandr Strizhnev on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetalUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
