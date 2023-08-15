e7ae8fdd13b7e453f616d0c976f7e1ac28e02875
Aleksandr Strijnev <alexstrnik@yandex-team.ru>
2023-08-16 00:43:21 +0300

    first commit

---
+++ MetalUI/MetalImage.swift
@@ -0,0 +1,20 @@
+//
+//  MetalImage.swift
+//  MetalUI
+//
+//  Created by Aleksandr Strizhnev on 15.08.2023.
+//
+
+import SwiftUI
+
+struct MetalImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MetalImage_Previews: PreviewProvider {
+    static var previews: some View {
+        MetalImage()
+    }
+}
---
+++ MetalUI/Painting.metal
@@ -0,0 +1,11 @@
+//
+//  Painting.metal
+//  MetalUI
+//
+//  Created by Aleksandr Strizhnev on 15.08.2023.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
